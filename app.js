92adf04391c594c285177d0f970ca900fb407b1b
Abhishek <abhishekabhi.r.2001@gmail.com>
2024-03-01 13:21:57 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+document.getElementById("addTask").addEventListener("click", function() {
+    var taskInput = document.getElementById("taskInput");
+    var taskList = document.getElementById("taskList");
+    var taskText = taskInput.value.trim();
+  
+    if (taskText !== "") {
+      var li = document.createElement("li");
+      li.textContent = taskText;
+      taskList.appendChild(li);
+      taskInput.value = "";
+  
+      li.addEventListener("click", function() {
+        this.classList.toggle("done");
+      });
+  
+      var deleteButton = document.createElement("span");
+      deleteButton.textContent = " ❌";
+      deleteButton.classList.add("delete");
+      li.appendChild(deleteButton);
+  
+      deleteButton.addEventListener("click", function(e) {
+        e.stopPropagation();
+        taskList.removeChild(li);
+      });
+    }
+  });
